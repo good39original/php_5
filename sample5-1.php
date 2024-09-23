7ff377466fc381ded16858b2d6bbcf2bd779d477
Sei <test@test.co.jp>
2024-09-24 01:02:36 +0900

    second commit

--- sample5-1.php
+++ sample5-1.php
@@ -47,8 +47,20 @@
           print $num;
 
           echo "<br>";
-
           
+          $a = 2;
+          $b = 3;
+          $c = 5;
+          $sum = 0;
+          function change($a, $b, $c) {
+            $a = 100;
+            global $b;
+            $b = 200;
+            $GLOBALS['c'] = 300;
+          }
+          change($a, $b, $c);
+          $sum = $a + $b + $c;
+          print $sum;
       ?>
     </p>
   </body>
